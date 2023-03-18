828543d7ff1d4c6a84d0f8a7d6b57ac1219c5d52
Jake Filipowski <filipowskijm@gmail.com>
2023-03-18 13:53:21 -0400

    added blank initial color array, color validity check

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import ColorBlock from './ColorBlock';
 import ColorForm from './ColorForm';
 
 function App() {
-  let [colors, setColors] = useState([
-    'violet', 'blue',
-    'lightblue', 'green',
-    'greenyellow', 'yellow',
-    'orange', 'red'])
+  let [colors, setColors] = useState([])
+    // 'violet', 'blue',
+    // 'lightblue', 'green',
+    // 'greenyellow', 'yellow',
+    // 'orange', 'red'])
   let colorMap = colors.map((color, i) => {
     return (
       <ColorBlock color={color} key={i} />
